6bca163e5cfb6a10bc97e9a304d7310a9ef2824a
soufiane <soufiane.oucherrou@gmail.com>
2019-09-24 14:18:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,8 +57,7 @@ def checkout(cart, coupons)
   after_coupons = apply_coupons(new_cart,coupons)
   after_clearance = apply_clearance(after_coupons)
   after_clearance.reduce(0) do |acu, (k,v)|
-    acu+= v[:price]
-    acu
+    
     binding.pry
   end
 end
